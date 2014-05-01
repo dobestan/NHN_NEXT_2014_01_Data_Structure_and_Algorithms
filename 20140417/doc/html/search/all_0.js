da05afe38a923ae3a6b2eb933e24d6eb1fc2041e
dobestan <dobestan@gmail.com>
2014-05-01 11:53:10 +0900

    complete binary search with duplicated values

--- 20140417/doc/html/search/all_0.js
+++ 20140417/doc/html/search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['binary_5fsearch',['binary_search',['../hw07__04_8c.html#a2a01d66e5bd59eb60af168fbdea79c12',1,'hw07_04.c']]],
   ['binarysearch',['binarySearch',['../hw07__03_8c.html#ae2490cbc85387186b1359e46ccc3c0dc',1,'hw07_03.c']]],
   ['bug_20list',['Bug List',['../bug.html',1,'']]]
 ];
--- 20140417/hw07_04.c
+++ 20140417/hw07_04.c
@@ -1,28 +1,49 @@
 /**
  * @file hw07_04.c
- * @brief NOT_IMPLEMENTED
+ * @brief Binary Search with duplicated values
  */
 
 #include <stdio.h>
 
 void printList(int list[], int size);
-int binarySearch(int list[], int left, int right, int key);
+int binary_search(int list[], int left, int right, int key);
+
+int search_left_most(int * list, int left, int right, int key);
+int search_right_most(int * list, int left, int right, int key);
+int get_num_of_key(int * list, int left, int right, int key);
+
+void integration_test(void);
+void unit_test(int * list, int left, int right, int key);
+
+void integration_test(void) {
+  int foo[5] = {1, 1, 1, 1, 1};
+  unit_test(foo, 0, sizeof(foo)/sizeof(foo[0])-1, 1);
+
+  int bar[5] = {1, 2, 2, 2, 5};
+  unit_test(bar, 0, sizeof(bar)/sizeof(bar[0])-1, 5);
+
+  int baz[10] = {1, 2, 5, 6, 8, 8, 8, 8, 8, 8};
+  unit_test(baz, 0, sizeof(baz)/sizeof(baz[0])-1, 8);
+}
+
+void unit_test(int * list, int left, int right, int key) {
+  printList(list, right+1);
+  printf("looking for ... [%d]\n", key);
+
+  int pos;
+  if ( ( pos = binary_search(list, 0, right+1, key) ) != -1 ) {
+    printf("gotcha! key is at [%d].\n", pos);
+    printf("Umm... the left most key is at [%d]\n", search_left_most(list, 0, right, key));
+    printf("Umm... the right most key is at [%d]\n", search_right_most(list, 0, right, key));
+    printf("So... the number of [%d] is [%d]\n", key, get_num_of_key(list, 0, right, key));
+  }
+  else
+    printf("[%d]를 찾을 수 없습니다.\n", key);
+}
+
 
 int main() {
-  int key; /// key는 찾고자 하는 값을 의미한다.
-  int list[10] = {1, 3, 4, 5, 6, 7, 8, 9, 12, 20};
-  int size = sizeof(list) / sizeof(int);
-  int pos;
-
-  printList(list, size);
-
-  key = 3;
-  if ( ( pos = binarySearch(list, 0, size-1, key) ) != -1 )
-    printf("key is at %d.\n", pos);
-  else
-    printf("key %d not found\n", key);
-
-  return 0;
+  integration_test();
 }
 
 void printList(int list[], int size) {
@@ -33,14 +54,14 @@ void printList(int list[], int size) {
 }
 
 /**
- * @fn int binarySearch(int list[], int left, int right, int key)
+ * @fn int binary_search(int list[], int left, int right, int key)
  * @brief binary search implementation
  * @details
  * @return
  * - key값이 존재하면 (int) position 리턴
  * - key값이 존재하지 않으면 -1 리턴
  */
-int binarySearch(int list[], int left, int right, int key) {
+int binary_search(int list[], int left, int right, int key) {
   /**
    * initial state
    *  - left = 0
@@ -56,4 +77,32 @@ int binarySearch(int list[], int left, int right, int key) {
   }
 
   return -1;
+}
+
+int search_left_most(int * list, int left, int right, int key) {
+  int left_most_index = binary_search(list, left, right, key);
+  if ( left_most_index == -1 ) return -1;
+
+  while ( list[left_most_index - 1] == key && left_most_index != 0 ) {
+    left_most_index--;
+  }
+  return left_most_index;
+}
+
+int search_right_most(int * list, int left, int right, int key) {
+  int right_most_index = binary_search(list, left, right, key);
+  if ( right_most_index == -1 ) return -1;
+
+  while ( list[right_most_index + 1] == key ) {
+    right_most_index++;
+  }
+  return right_most_index;
+}
+
+
+int get_num_of_key(int * list, int left, int right, int key) {
+  int left_most_index = search_left_most(list, left, right, key);
+  int right_most_index = search_right_most(list, left, right, key);
+  if ( left_most_index == -1 || right_most_index == -1 ) return -1;
+  return right_most_index - left_most_index + 1;
 }
